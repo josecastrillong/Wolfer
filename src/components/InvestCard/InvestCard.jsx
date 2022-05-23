6e5b39153b1c469d2f3a53e9f6b2f9a0b4b3c018
jose_miguel <josecastrillong@gmail.com>
2022-05-23 10:40:31 -0500

    property review component sketch

---
+++ src/components/InvestCard/InvestCard.jsx
@@ -0,0 +1,17 @@
+import InvestButton from '../Buttons/Invest/Invest';
+
+function InvestCard() {
+  return (
+    <div>
+      <div>
+        <span>$1.000.000</span>
+        <span>/ bloque</span>
+      </div>
+      <div>
+        <InvestButton />
+      </div>
+    </div>
+  );
+}
+
+export default InvestCard;
--- src/components/PropertyReview/PropertyReview.jsx
+++ src/components/PropertyReview/PropertyReview.jsx
@@ -1,4 +1,9 @@
+// import { useEffect, useState } from 'react';
 import ArchitectureIcon from '@mui/icons-material/Architecture';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import DescriptionIcon from '@mui/icons-material/Description';
+import { Progress } from 'react-sweet-progress';
+import TokenIcon from '@mui/icons-material/Token';
 import styles from './styles.module.css';
 
 function PropertyReview() {
@@ -9,15 +14,40 @@ function PropertyReview() {
           Casa Aguas Claras
         </h2>
         <div className={styles.qualifications}>
-          5 estrellas
+          <h3>Medellín - Colombia</h3>
         </div>
         <div className={styles.investors}>
-          120
+          120 inversionistas
         </div>
         <hr />
         <div>
-          <div className={styles.icon}>
-            {ArchitectureIcon}
+          <div className={styles.info_section}>
+            <ArchitectureIcon />
+            <div className={styles.text_info}>
+              <h3>Diseño</h3>
+              <p>Rogelio Salmona</p>
+            </div>
+          </div>
+          <div className={styles.info_section}>
+            <EngineeringIcon />
+            <div className={styles.text_info}>
+              <h3>Constructor</h3>
+              <p>Alguna constructora</p>
+            </div>
+          </div>
+          <div className={styles.info_section}>
+            <DescriptionIcon />
+            <div className={styles.text_info}>
+              <h3>Espacios</h3>
+              <p>Rogelio Salmona</p>
+            </div>
+          </div>
+          <div className={styles.info_section}>
+            <TokenIcon />
+            <div className={styles.text_info}>
+              <h3>Bloques comprados</h3>
+              <Progress percent={50} />
+            </div>
           </div>
         </div>
       </div>
