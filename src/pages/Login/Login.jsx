e009ad2c95f964259fece5cae47f41948f5bc041
jose_miguel <josecastrillong@gmail.com>
2022-06-08 00:22:57 -0500

    bug-fix in login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,4 @@
 // Import FirebaseAuth and firebase.
-import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
@@ -8,7 +7,8 @@ import styles from './styles.module.css';
 // Configure Firebase.
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // eslint-disable-next-line no-undef
+  authDomain: wolfer.vercel.app,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
