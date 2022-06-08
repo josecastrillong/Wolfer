841c09741fa0edc62fbe492aa4053bc064f0550c
jose_miguel <josecastrillong@gmail.com>
2022-06-08 09:46:09 -0500

    changes auth providers

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -16,9 +16,6 @@ const uiConfig = {
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
   ],
 };
 
